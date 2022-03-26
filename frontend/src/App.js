a26a25ff6876fcafabf252adc28af2f295ac237e
wongliping <w.wongliping@gmail.com>
2022-03-26 19:18:35 +0800

    add product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
                 <Container>
                     <Routes>
                         <Route path="/" element={<HomeScreen />} exact />
-                        <Route path="/product/:id" element={<ProductScreen />} />
+                        <Route path="/product/:id" element={<ProductScreen />} exact />
                     </Routes>
                 </Container>
             </main>
