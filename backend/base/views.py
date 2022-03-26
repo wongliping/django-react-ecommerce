d2537d7f56c3bba82b9bcf91e82140e269ef6077
wongliping <w.wongliping@gmail.com>
2022-03-26 22:10:08 +0800

    add views for products

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+
 
 # Create your views here.
 
 
+@api_view(["GET"])
 def getRoutes(request):
     routes = [
         "/api/products/",
@@ -15,4 +20,19 @@ def getRoutes(request):
         "/api/products/delete/<id>/",
         "/api/products/<update>/<id>",
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(["GET"])
+def getProduct(request, pk):
+    product = None
+    for p in products:
+        if p['_id'] == pk:
+            product = p
+            break
+    return Response(product)
